2dd6c399a515c004ed25cc18f84bf2dc3e9273ef
jonathancmaia <jonathancmaia@gmail.com>
2023-05-19 13:32:28 -0300

    change paths to git hub pages

--- src/App.js
+++ src/App.js
@@ -21,12 +21,12 @@ function App() {
           <Header />
           <main className='AppBody'>
             <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='login/*' element={<Login />} />
-              <Route path='conta/*' element={<ProtectedRoute> <User /> </ProtectedRoute>} />
-              <Route path='foto/:id' element={<Photo />} />
-              <Route path='perfil/:user' element={<UserProfile />} />
-              <Route path='*' element={<NotFound />} />
+              <Route path='dogsgram/' element={<Home />} />
+              <Route path='dogsgram/login/*' element={<Login />} />
+              <Route path='dogsgram/conta/*' element={<ProtectedRoute> <User /> </ProtectedRoute>} />
+              <Route path='dogsgram/foto/:id' element={<Photo />} />
+              <Route path='dogsgram/perfil/:user' element={<UserProfile />} />
+              <Route path='dogsgram/*' element={<NotFound />} />
             </Routes>
           </main>
           <Footer />
